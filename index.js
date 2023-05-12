c270a57bc9ca8b5a2aa7683b4c3a7274e9b3235c
juanma <juanaco1.p@gmail.com>
2023-05-12 10:15:50 +0200

    envpoint working

--- index.js
+++ index.js
@@ -1,17 +1,81 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
+import dayjs from 'dayjs';
+import * as chrono from 'chrono-node';
 
+dotenv.config();
 const app = express();
 
-app.get('/', (req, res) => {
-  if ( !req.query.text ) {
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const stringToDateString = (string, timezone = 'Etc/GMT') => {
+  const localTime = dayjs(new Date(), timezone)
+  const localOffset = localTime.utcOffset();
+  const custom = chrono.casual.clone();
+  custom.refiners.push({
+    refine: (context, results) => {
+      results.forEach((result) => {
+        result.start.imply('timezoneOffset', localOffset)
+        result.end && result.end.imply('timezoneOffset', localOffset)
+      })
+      return results
+    }
+  });
+  const date = custom.parseDate(string);
+  if (!date) return null;
+  return dayjs(date).toISOString();
+};
+
+
+app.get('/', async (req, res) => {
+  const text = req.query.text;
+
+  if (!text) {
     return res.status(400).json({
       error: 'Missing required parameter `text`',
     });
   }
 
-  res.json({
-    text: req.query.text,
-  });
+  try {
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: `Vamos a parsear frases. La respuesta debe de ser un JSON valido con las siguientes propiedades:
+            temp: la parte de la frase que se refiere al tiempo, es decir, la indicacion temporal de la frase
+            temp_en: la parte de la frase que se refiere al tiempo, es decir, la indicacion temporal de la frase. Traducela a ingles
+            rest: la parte restante de la frase. Lo que queda al extraer la indicacion temporal de la frase original
+            frase: "tengo dentista mañana a las 10"
+            respuesta: { "temp": "mañana a las 10", "temp_en": "tomorrow at 10 a.m.", "rest": "tengo dentista" }
+            frase: "ayer cene con mi amigo por la noche"
+            respuesta: { "temp": "ayer por la noche", "temp_en": "last night", "rest": "cene con mi amigo" }
+            frase: "1 en un minuto"
+            respuesta: { "temp": "en un minuto", "temp_en": "in a minute", "rest": "1" }
+            frase: "${text}"
+            respuesta:`,
+        },
+      ],
+      temperature: 0,
+    });
+    const content = JSON.parse(completion.data.choices[0].message.content);
+
+    return res.json({
+      original_text: text,
+      temporal_text: content.temp,
+      rest_text: content.rest,
+      date: stringToDateString(content.temp_en),
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: 'Server error',
+    });
+  }
 });
 
 app.listen(3000, () => {
