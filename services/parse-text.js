3ae0419cb55c69d8ad8f2ca0a1f0c029367380b6
juanma <juanaco1.p@gmail.com>
2023-05-18 09:49:44 +0200

    prompt modify

--- services/parse-text.js
+++ services/parse-text.js
@@ -8,23 +8,26 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export const parseText = async (text) => {
+  const prompt = `Vamos a parsear frases. R debe de ser un JSON valido con las siguientes propiedades:
+  temporal_text: la parte de P que se refiere al tiempo, es decir, la indicacion temporal de P
+  temp_en: la parte de P que se refiere al tiempo, es decir, la indicacion temporal de P. Traducela a ingles
+  rest: la parte restante de P. Lo que queda al extraer la indicacion temporal de P original
+
+  P: "tengo dentista mañana a las 10"
+  R: { "temporal_text": "mañana a las 10", "temp_en": "tomorrow at 10 a.m.", "rest": "tengo dentista" }
+
+  P: "ayer cene con mi amigo por la noche"
+  R: { "temporal_text": "ayer por la noche", "temp_en": "last night", "rest": "cene con mi amigo" }
+
+  P: "1 en un minuto"
+  R: { "temporal_text": "en un minuto", "temp_en": "in a minute", "rest": "1" }`;
+
   const completion = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages: [
       {
         role: 'user',
-        content: `Vamos a parsear frases. La respuesta debe de ser un JSON valido con las siguientes propiedades:
-          temporal_text: la parte de la frase que se refiere al tiempo, es decir, la indicacion temporal de la frase
-          temp_en: la parte de la frase que se refiere al tiempo, es decir, la indicacion temporal de la frase. Traducela a ingles
-          rest: la parte restante de la frase. Lo que queda al extraer la indicacion temporal de la frase original
-          frase: "tengo dentista mañana a las 10"
-          respuesta: { "temporal_text": "mañana a las 10", "temp_en": "tomorrow at 10 a.m.", "rest": "tengo dentista" }
-          frase: "ayer cene con mi amigo por la noche"
-          respuesta: { "temporal_text": "ayer por la noche", "temp_en": "last night", "rest": "cene con mi amigo" }
-          frase: "1 en un minuto"
-          respuesta: { "temporal_text": "en un minuto", "temp_en": "in a minute", "rest": "1" }
-          frase: "${text}"
-          respuesta:`,
+        content: prompt + `\nP: "${text}"\nR: `,
       },
     ],
     temperature: 0,
